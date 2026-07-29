90ae460c5b3ba8656dd363c3ef4ce3914e511160
AhmedDip <ahmedrb.dip@gmail.com>
2026-07-29 14:37:53 +0600

    Added

--- config/services.php
+++ config/services.php
@@ -67,7 +67,7 @@ return [
         'ecm_url'      => env('BANGLALINK_ECM_URL', 'https://corpsms.banglalink.net/bl/ecmapigw/webresources/ecmapigw.v3'),
         'username'     => env('BANGLALINK_SMS_USERNAME'),
         'password'     => env('BANGLALINK_SMS_PASSWORD'),
-        'cli' => config('services.banglalink.cli', 'No.1'),
+         'cli'          => env('BANGLALINK_SMS_CLI', 'No.1'),
         'country_code' => env('BANGLALINK_SMS_COUNTRY_CODE', '880'),
         'rn_code'      => env('BANGLALINK_SMS_RN_CODE', '91'),
         // apicode 5 = send with delivery receipt, 6 = send without delivery receipt
